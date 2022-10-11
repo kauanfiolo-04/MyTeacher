abfdfdc8b79d20198e1df9104249e00ef324cc91
kauanfiolo-04 <kauanfiolo@hotmail.com>
2022-10-11 18:21:37 -0300

    .

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,15 +1,38 @@
-import { Box } from '@mui/material'
+import { Box, Dialog, Grid, TextField } from '@mui/material'
 import type { NextPage } from 'next'
 import List from '../src/components/List/List'
 import { useIndex } from '../src/hooks/pages/useIndex'
 
 const Home: NextPage = () => {
-  const { listProfessors }= useIndex();
+  const { listProfessors } = useIndex();
 
   return (
-   <Box sx={{backgroundColor: 'secondary.main'}}>
-    <List professors={listProfessors}></List>
-   </Box>
+    <div>
+      <Box sx={{ backgroundColor: 'secondary.main' }}>
+        <List professors={listProfessors}></List>
+      </Box>
+
+      <Dialog open={true} fullWidth PaperProps={{ sx: { p: 5 } }}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <TextField
+              label="Digite o nome "
+              type="text"
+              fullWidth
+            />
+          </Grid>
+  
+          <Grid item xs={12}>
+            <TextField
+              label="Digite o e-mail"
+              type="email"
+              fullWidth
+            />
+          </Grid>
+
+        </Grid>
+      </Dialog>
+    </div>
   )
 }
 
